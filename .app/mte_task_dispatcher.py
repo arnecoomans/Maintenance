6563f9df9528efa753e088548cbdca9608176bb9
Arne Coomans <git@arnecoomans.nl>
2020-12-20 11:27:23 +0000

    improved handling of no tasks enabled

--- .app/mte_task_dispatcher.py
+++ .app/mte_task_dispatcher.py
@@ -32,7 +32,11 @@ class TaskDispatcher:
       if os.path.splitext(filename)[1] == ".py":
         if self.is_marked_as_task(filename):
           self.available_tasks.append(os.path.splitext(filename)[0])
-    self.core.log.add("Found available tasks: [" + ", ".join(self.available_tasks) + "].", 5)
+    
+    if len(self.available_tasks) == 0:
+      self.core.log.add('No tasks found in tasks-enabled/. Please check documentation in docs/tasks.md to enable tasks.', 1)
+    else:
+      self.core.log.add("Found available tasks: [" + ", ".join(self.available_tasks) + "].", 5)
   
   def is_marked_as_task(self, filename):
     # Tasks should have "# [MTETASK]" as second line in the file
